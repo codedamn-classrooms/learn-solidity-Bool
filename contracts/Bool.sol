b2bfa40ea314c29e95b1b204c78be95725a3d025
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-06-21 22:22:36 +0530

    Update Bool.sol

--- contracts/Bool.sol
+++ contracts/Bool.sol
@@ -3,11 +3,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Bool {
- function check (bool _value) public pure returns(string memory){
-    if(_value){
-       return "The value is true";
-    }else{
-       return "The value is false";
-    }
- }
+
 }
